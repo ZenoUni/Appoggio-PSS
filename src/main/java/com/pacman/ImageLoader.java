f2ce2b56f1b64960ba4a352073dff424c54877c0
AlecUnibo <alessandro.cacchi@studio.unibo.it>
2025-05-15 09:31:36 +0200

    Sistemato SoundManger + Edit Immagini

--- src/main/java/com/pacman/ImageLoader.java
+++ src/main/java/com/pacman/ImageLoader.java
@@ -9,10 +9,16 @@ public class ImageLoader {
     private final Image pacmanLeftImage;
     private final Image pacmanRightImage;
     private final Image pacmanCloseImage;
+
+    private final Image pacmanDeath1Image;
+    private final Image pacmanDeath2Image;
+    private final Image pacmanDeath3Image;
+
     private final Image blueGhostImage;
     private final Image orangeGhostImage;
     private final Image pinkGhostImage;
     private final Image redGhostImage;
+
     private final Image powerFoodImage;
     private final Image cherryImage;
     private final Image appleImage;
@@ -28,6 +34,10 @@ public class ImageLoader {
         pacmanRightImage = load("/assets/pacmanRight.png");
         pacmanCloseImage = load("/assets/pacmanClose.png");
 
+        pacmanDeath1Image = load("/assets/Death/pacmanDeath1.png");
+        pacmanDeath2Image = load("/assets/Death/pacmanDeath2.png");
+        pacmanDeath3Image = load("/assets/Death/pacmanDeath3.png");
+
         blueGhostImage   = load("/assets/blueGhost.png");
         orangeGhostImage = load("/assets/orangeGhost.png");
         pinkGhostImage   = load("/assets/pinkGhost.png");
@@ -35,7 +45,7 @@ public class ImageLoader {
 
         powerFoodImage   = load("/assets/powerFood.png");
         cherryImage      = load("/assets/cherry.png");
-        appleImage      = load("/assets/apple.png");
+        appleImage       = load("/assets/apple.png");
         strawberryImage  = load("/assets/strawberry.png");
         scaredGhostImage = load("/assets/scaredGhost.png");
         whiteGhostImage  = load("/assets/whiteGhost.png");
@@ -51,6 +61,10 @@ public class ImageLoader {
     public Image getPacmanLeftImage()     { return pacmanLeftImage; }
     public Image getPacmanRightImage()    { return pacmanRightImage; }
     public Image getPacmanClosedImage()   { return pacmanCloseImage; }
+
+    public Image getPacmanDeath1Image()   { return pacmanDeath1Image; }
+    public Image getPacmanDeath2Image()   { return pacmanDeath2Image; }
+    public Image getPacmanDeath3Image()   { return pacmanDeath3Image; }
 
     public Image getBlueGhostImage()      { return blueGhostImage; }
     public Image getOrangeGhostImage()    { return orangeGhostImage; }
